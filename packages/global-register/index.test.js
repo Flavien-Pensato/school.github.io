7d7ab6ea7d391f31957a28b436aa417160bb36a3
Flavien-Pensato <flavien.pensato@gmail.com>
2018-05-01 13:28:36 +0200

    feat(global registry): add get node

--- packages/global-register/index.test.js
+++ packages/global-register/index.test.js
@@ -1,4 +1,5 @@
 import GR from './index';
+import { notDeepEqual } from 'assert';
 
 describe('Core of global register', () => {
   describe('Assure immutabiliy', () => {
@@ -12,6 +13,45 @@ describe('Core of global register', () => {
       const GR2 = require('./index').default;
       
       expect(GR).toEqual(GR2);
+    });
+  });
+
+  describe('Test registration', () => {
+    it('should have save value', () => {
+      GR.register('test', 'test value');
+
+      expect(GR.getValue('test')).toEqual('test value');
+    });
+
+    it('should save number', () => {
+      GR.register('testNumber', 190);
+      
+      expect(GR.getValue('testNumber')).toEqual(190);
+    });
+
+    it('should save object', () => {
+      GR.register('testObject', { foo: 'foo', bar: 'bar'});
+      
+      expect(GR.getValue('testObject')).toEqual({ foo: 'foo', bar: 'bar'});
+    });
+
+    it('should throw key already set', () => {
+      expect(() => GR.register('testObject', { bob: 'bob', bar: 'bar'})).toThrowError('Key already register');
+    });
+
+    it('should set with force', () => {
+      expect(() => GR.register('testObject', { bob: 'bob', bar: 'bar'}, true)).not.toThrowError('Key already register');
+    });
+
+    it('should set with force', () => {
+      GR.register('routes.toto', 100);
+
+      expect(GR.getValue('routes.toto')).toEqual(100);
+    });
+
+    it('should get nodes', () => {
+      expect(GR.getNodes('.')).toEqual(["hello", "test", "testNumber", "testObject", "routes"]);
+      expect(GR.getNodes('routes')).toEqual(["toto"]);
     });
   });
 });
