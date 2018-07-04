2e8ac4dbdd207aa2c435fdc503212583b5d89e51
Flavien-Pensato <flavien.pensato@gmail.com>
2018-07-04 21:31:58 +0200

    chore(): forget to buil

--- packages/ui/.storybook/webpack.config.js
+++ packages/ui/.storybook/webpack.config.js
@@ -5,7 +5,9 @@ const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/w
 module.exports = (baseConfig, env) => {
   const config = genDefaultConfig(baseConfig, env);
 
-  if (env === 'PRODUCTION' && config.plugins[0].options) {
+  console.log(env)
+
+  if (env === 'PRODUCTION') {
     config.plugins[0].options.filename = 'storybook.html'
   }
 
