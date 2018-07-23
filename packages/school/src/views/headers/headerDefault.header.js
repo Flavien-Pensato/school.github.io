f720d6e5e39c08e8e3ebd5e345354bcda935148b
f-pensato <flavien.pensato@gmail.com>
2018-07-23 13:13:07 +0200

    fixup! fixup! feat(title): title for mobile

--- packages/school/src/views/headers/headerDefault.header.js
+++ packages/school/src/views/headers/headerDefault.header.js
@@ -10,8 +10,8 @@ import { getSelectedWeek } from '../../modules/calendar/calendar.selectors';
 
 const LogoutSvg = () => (
   <StyledLogoutSvg>
-    <path d="M0 0h24v24H0z" fill="none"/>
-    <path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" />
   </StyledLogoutSvg>
 );
 
@@ -24,7 +24,7 @@ const StyledLogoutSvg = styled.svg`
   @media (max-width: 700px) {
     display: inherit;
   }
-`
+`;
 
 const HeaderContent = styled.header`
   justify-content: center;
@@ -59,7 +59,7 @@ export const HeaderDefault = ({ currentWeek, signOutAction }) => (
   <HeaderContent>
     <HeaderWrapper>
       <HeaderTitle>Planning de la MFR de chatte</HeaderTitle>
-      <LogoutButton onClick={signOutAction}><span>Se déconnecter</span><LogoutSvg/></LogoutButton>
+      <LogoutButton onClick={signOutAction}><span>Se déconnecter</span><LogoutSvg /></LogoutButton>
     </HeaderWrapper>
     <HeaderNav>
       <HeaderLink to="/home">
