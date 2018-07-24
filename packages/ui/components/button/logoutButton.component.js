530494b5904910c6acdda98b46e4e677d433600b
f-pensato <flavien.pensato@gmail.com>
2018-07-24 15:43:03 +0200

    feat(menu): intergrate mobile menu

--- packages/ui/components/button/logoutButton.component.js
+++ packages/ui/components/button/logoutButton.component.js
@@ -2,11 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LogoutButtonStyle = styled.button`
-  position: absolute;
   font-size: 0.875rem;
   font-weight: 700;
-  top: 20px;
-  right: 20px;
 
   @media (max-width: 700px) {
     position: inherit;
