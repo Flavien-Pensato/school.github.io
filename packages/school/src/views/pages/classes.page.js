d3e79cc33af49293f0cf0dc8439023850977059b
Flavien Pensato <Flavien-Pensato@users.noreply.github.com>
2018-07-29 18:59:24 +0200

    refactor(classes): rename students by classes (#10)

--- packages/school/src/views/pages/classes.page.js
+++ packages/school/src/views/pages/classes.page.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { ConnectedClassesList } from '../../modules/classes/components/classesList.connector';
 import { ConnectedClassesForm } from '../../modules/classes/components/classeForm.connector';
 
-export const Students = () => (
+export const Classes = () => (
   <div className="pa3 pa5-ns">
     <ConnectedClassesList />
     <ConnectedClassesForm />
