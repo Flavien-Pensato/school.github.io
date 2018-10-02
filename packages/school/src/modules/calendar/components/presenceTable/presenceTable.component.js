918e56787fa9987d0cff010f25680b1120f58da7
f-pensato <flavien.pensato@gmail.com>
2018-10-02 17:40:07 +0200

    feat(calendar): fix header and left column

--- packages/school/src/modules/calendar/components/presenceTable/presenceTable.component.js
+++ packages/school/src/modules/calendar/components/presenceTable/presenceTable.component.js
@@ -10,16 +10,49 @@ import { uuidv4 } from '../../../../utils';
 
 moment.locale('fr');
 
-const Container = styled.table`
+const Container = styled.div`
+  display: grid;
   overflow: auto;
-  margin: 0 auto;
-  font-size: .875rem;
+  height: 300px;
+  width: 600px;
+`;
 
-  display: block;
+const Grid = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
 
-  width: 100%;
-  max-width: 64rem;
-  max-height: 32rem;
+const Col = styled.div`
+  width: 150px;
+  min-width: 150px;
+`;
+
+const ItemHeader = styled.div`
+  height: 100px;
+  min-height: 100px;
+  position: sticky;
+  position: -webkit-sticky;
+  background: white;
+  top: 0;
+`;
+
+const ColFixedLeft = styled.div`
+  position: sticky;
+  left: 0;
+  z-index: 9998;
+  background: white;
+`;
+
+const ColFixedRight = styled.div`
+  position: sticky;
+  right: 0;
+  z-index: 9998;
+  background: white;
+`;
+
+const Item = styled.div`
+  height: 50px;
+  border: 1px solid gray;
 `;
 
 export class PresenceTable extends Component {
@@ -58,37 +91,39 @@ export class PresenceTable extends Component {
     return (
       <div className="pa4">
         <Container>
-          <thead>
-            <tr>
-              <th className="fw6 bb b--black-20 pb3 pr3 tl">Semaine</th>
-              {classesSorted.map(classe => (
-                <th className="fw6 bb b--black-20 pb3 pr3" key={classe.name}>{classe.name}</th>
-                ))}
-            </tr>
-          </thead>
-          <tbody className="lh-copy tc">
-            {datesSorted.map(date => (
-              <tr key={date._id}>
-                <td className="pv3 pr3 bb b--black-20 tl">
-                    Du&nbsp;{moment(date.from, 'YYYY.MM.DD').format('dddd D MMMM')}
-                  <br />
-                    Au&nbsp;{moment(date.to, 'YYYY.MM.DD').format('dddd D MMMM')}
-                </td>
+          <Grid>
+            <thead>
+              <tr>
+                <th className="fw6 bb b--black-20 pb3 pr3 tl">Semaine</th>
                 {classesSorted.map(classe => (
-                  <PresenceCase
-                    editDate={editDate}
-                    key={classe._id}
-                    date={date}
-                    presence={date.classes.includes(classe._id)}
-                    classeId={classe._id}
-                  />
-                  ))}
+                  <th className="fw6 bb b--black-20 pb3 pr3" key={classe.name}>{classe.name}</th>
+                ))}
               </tr>
+            </thead>
+            <tbody className="lh-copy tc">
+              {datesSorted.map(date => (
+                <tr key={date._id}>
+                  <td className="pv3 pr3 bb b--black-20 tl">
+                    Du&nbsp;{moment(date.from, 'YYYY.MM.DD').format('dddd D MMMM')}
+                    <br />
+                    Au&nbsp;{moment(date.to, 'YYYY.MM.DD').format('dddd D MMMM')}
+                  </td>
+                  {classesSorted.map(classe => (
+                    <PresenceCase
+                      editDate={editDate}
+                      key={classe._id}
+                      date={date}
+                      presence={date.classes.includes(classe._id)}
+                      classeId={classe._id}
+                    />
+                  ))}
+                </tr>
               ))}
-            <tr>
-              <button onClick={this.handleAddDate}>+</button>
-            </tr>
-          </tbody>
+              <tr>
+                <button onClick={this.handleAddDate}>+</button>
+              </tr>
+            </tbody>
+          </Grid>
         </Container>
       </div>
     );
