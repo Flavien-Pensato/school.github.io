17c3f6bb5b03881533e4e4380f78d617ab550eac
Flavien Pensato <Flavien-Pensato@users.noreply.github.com>
2019-08-21 14:07:56 +0200

    fix(): linter

--- packages/ui/lib/index.js
+++ packages/ui/lib/index.js
@@ -1,110 +1,24 @@
-'use strict';
+"use strict";
 
-Object.defineProperty(exports, "__esModule", {
+var _Object$defineProperty = require("@babel/runtime-corejs2/core-js/object/define-property");
+
+_Object$defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _button = require('./button/button.component');
-
-Object.defineProperty(exports, 'Button', {
-  enumerable: true,
-  get: function get() {
-    return _button.Button;
-  }
-});
-
-var _purpleButton = require('./button/purpleButton.component');
-
-Object.defineProperty(exports, 'PurpleButton', {
-  enumerable: true,
-  get: function get() {
-    return _purpleButton.PurpleButton;
-  }
-});
-
-var _logoutButton = require('./button/logoutButton.component');
-
-Object.defineProperty(exports, 'LogoutButton', {
-  enumerable: true,
-  get: function get() {
-    return _logoutButton.LogoutButton;
-  }
-});
-
-var _calendarButton = require('./button/calendarButton.component');
-
-Object.defineProperty(exports, 'CalendarButton', {
-  enumerable: true,
-  get: function get() {
-    return _calendarButton.CalendarButton;
-  }
-});
-
-var _input = require('./input/input.component');
-
-Object.defineProperty(exports, 'Input', {
+_Object$defineProperty(exports, "Input", {
   enumerable: true,
   get: function get() {
     return _input.Input;
   }
 });
-Object.defineProperty(exports, 'InputForm', {
+
+_Object$defineProperty(exports, "InputForm", {
   enumerable: true,
   get: function get() {
     return _input.InputForm;
   }
 });
 
-var _legend = require('./legend/legend.component');
-
-Object.defineProperty(exports, 'Legend', {
-  enumerable: true,
-  get: function get() {
-    return _legend.Legend;
-  }
-});
-
-var _title = require('./title/title.component');
-
-Object.defineProperty(exports, 'Title', {
-  enumerable: true,
-  get: function get() {
-    return _title.Title;
-  }
-});
-
-var _headerTitle = require('./title/headerTitle.component');
-
-Object.defineProperty(exports, 'HeaderTitle', {
-  enumerable: true,
-  get: function get() {
-    return _headerTitle.HeaderTitle;
-  }
-});
-
-var _form = require('./form/form.component');
-
-Object.defineProperty(exports, 'Form', {
-  enumerable: true,
-  get: function get() {
-    return _form.Form;
-  }
-});
-
-var _headerLink = require('./headerLink/headerLink.component');
-
-Object.defineProperty(exports, 'HeaderLink', {
-  enumerable: true,
-  get: function get() {
-    return _headerLink.HeaderLink;
-  }
-});
-
-var _wrapper = require('./wrapper/wrapper.style');
-
-Object.defineProperty(exports, 'Wrapper', {
-  enumerable: true,
-  get: function get() {
-    return _wrapper.Wrapper;
-  }
-});
+var _input = require("./components/input/input.component");
+//# sourceMappingURL=index.js.map
