1adef9e9c81775554bf4c615180f56569aadfdb7
Flavien Pensato <flavien.pensato@itsfactory.fr>
2024-08-26 08:43:17 +0200

    feat(): new year start

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,56 +1,56 @@
-import db from '../../utils/db';
-import Week from '../../modules/week/week.model';
-import Student from '../../modules/students/student.model';
+import db from "../../utils/db";
+import Week from "../../modules/week/week.model";
+import Student from "../../modules/students/student.model";
 
 export default async function handler(req, res) {
-  await db();
+	await db();
 
-  try {
-    const classes = await Student.distinct('classe');
-    /* find all the data in our database */
-    const weeks = await Week.find({
-      startAt: {
-        $gt: new Date('08/30/2023'),
-      },
-      endAt: {
-        $lt: new Date('07/01/2024'),
-      },
-    });
+	try {
+		const classes = await Student.distinct("classe");
+		/* find all the data in our database */
+		const weeks = await Week.find({
+			startAt: {
+				$gt: new Date("08/30/2024"),
+			},
+			endAt: {
+				$lt: new Date("07/01/2025"),
+			},
+		});
 
-    const stats = weeks.reduce((acc, value) => {
-      if (value.tasks) {
-        Object.keys(value.tasks).forEach((taskName) => {
-          const task = value.tasks[taskName];
-          const groupe = acc[task.groupe];
+		const stats = weeks.reduce((acc, value) => {
+			if (value.tasks) {
+				Object.keys(value.tasks).forEach((taskName) => {
+					const task = value.tasks[taskName];
+					const groupe = acc[task.groupe];
 
-          if (!groupe) {
-            acc[task.groupe] = {
-              total: 0,
-              classe: 0,
-            };
-          }
+					if (!groupe) {
+						acc[task.groupe] = {
+							total: 0,
+							classe: 0,
+						};
+					}
 
-          if (classes.includes(taskName)) {
-            if (!acc[task.groupe][taskName]) {
-              acc[task.groupe][taskName] = 1;
-              acc[task.groupe].classe += 1;
-            } else {
-              acc[task.groupe][taskName] += 1;
-              acc[task.groupe].classe += 1;
-            }
-          } else if (!acc[task.groupe][taskName]) {
-            acc[task.groupe][taskName] = 1;
-          } else {
-            acc[task.groupe][taskName] += 1;
-          }
-          acc[task.groupe].total += 1;
-        });
-      }
+					if (classes.includes(taskName)) {
+						if (!acc[task.groupe][taskName]) {
+							acc[task.groupe][taskName] = 1;
+							acc[task.groupe].classe += 1;
+						} else {
+							acc[task.groupe][taskName] += 1;
+							acc[task.groupe].classe += 1;
+						}
+					} else if (!acc[task.groupe][taskName]) {
+						acc[task.groupe][taskName] = 1;
+					} else {
+						acc[task.groupe][taskName] += 1;
+					}
+					acc[task.groupe].total += 1;
+				});
+			}
 
-      return acc;
-    }, {});
-    res.status(200).json({ success: true, data: stats });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+			return acc;
+		}, {});
+		res.status(200).json({ success: true, data: stats });
+	} catch (error) {
+		res.status(400).json({ error });
+	}
 }
